af7b59730947e3dac5c197a4df8935574a842507
Adnan <adnan.muhib@rocketmail.com>
2018-11-18 03:10:23 +0500

    :100: Add: Background Subtraction from the Given Two Images

---
+++ GaitRecognition/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GaitRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureViewBox = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureViewBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(997, 483);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 483);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(797, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 483);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureViewBox);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(200, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(597, 483);
+            this.panel4.TabIndex = 2;
+            // 
+            // pictureViewBox
+            // 
+            this.pictureViewBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureViewBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureViewBox.Name = "pictureViewBox";
+            this.pictureViewBox.Size = new System.Drawing.Size(597, 483);
+            this.pictureViewBox.TabIndex = 2;
+            this.pictureViewBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(200, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processToolStripMenuItem1});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.processToolStripMenuItem.Text = "Tools";
+            // 
+            // processToolStripMenuItem1
+            // 
+            this.processToolStripMenuItem1.Name = "processToolStripMenuItem1";
+            this.processToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.processToolStripMenuItem1.Text = "Process";
+            this.processToolStripMenuItem1.Click += new System.EventHandler(this.processToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 483);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Gait Recognition";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureViewBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private Emgu.CV.UI.ImageBox pictureViewBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem1;
+    }
+}
+
---
+++ GaitRecognition/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Emgu.CV;
+using Emgu.CV.BgSegm;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaitRecognition
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> img; // any input image
+        Image<Gray, byte> bgImage; // Background Image from the video
+
+        VideoCapture _capture; // to read video files
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            //batchProcessor();
+            removebackground();
+            _capture = new VideoCapture(@"D:\UNIVERSITY DOCUMENTS\FYP\Human Activity Recognition\KTH Dataset\walking_person_15.avi");
+            _capture.ImageGrabbed += _capture_ImageGrabbed;
+        }
+
+        private void _capture_ImageGrabbed(object sender, EventArgs e)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private void processToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // When Menu item Open is clicked
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialogue();
+        }
+
+        // Open File Selection Dialogue Box
+        public void openFileDialogue() {
+            OpenFileDialog f = new OpenFileDialog();
+            if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(f.FileName);
+                pictureViewBox.Image = img;
+            }
+        }
+
+        // when shortcut key of CTRL + O is pressed
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.O)
+            {
+                openFileDialogue();
+            }
+        }
+
+        // Batch Processor
+        public void batchProcessor() {
+            String inputFolderPath = @"D:\UNIVERSITY DOCUMENTS\FYP\Human Activity Recognition\KTH Dataset\Gait Pics\";
+            String outputFolderPath = @"D:\UNIVERSITY DOCUMENTS\FYP\Human Activity Recognition\Test Outputs\";
+            List<String> files = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(inputFolderPath);
+
+            FileInfo[] Files = d.GetFiles("*.bmp"); //Getting BMP files
+            foreach (FileInfo file in Files) {
+                files.Add(file.Name);
+            }
+
+            return;
+        }
+
+        public void removebackground(string filepath = null) {
+
+            String inputFolderPath = @"D:\UNIVERSITY DOCUMENTS\FYP\Human Activity Recognition\KTH Dataset\Gait Pics\";
+
+            bgImage = new Image<Gray, byte>(inputFolderPath + "b_2.bmp");
+            Image<Gray, byte> imgOrignal = new Image<Gray, byte>(inputFolderPath + "9.bmp");
+            Image<Gray, byte> output = new Image<Gray, byte>(bgImage.Width, bgImage.Height);
+
+            //BackgroundSubtractorMOG2 bgsubtractor = new BackgroundSubtractorMOG2(varThreshold:80,shadowDetection:false);
+            BackgroundSubtractorGSOC bgsubtractor = new BackgroundSubtractorGSOC(BackgroundSubtractorLSBP.CameraMotionCompensation.LK);
+            bgsubtractor.Apply(bgImage, output);
+            bgsubtractor.Apply(imgOrignal, output);
+            pictureViewBox.Image = output;
+        }
+    }
+}
