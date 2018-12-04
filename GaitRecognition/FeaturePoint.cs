a207f139abd4b2aa658d45fd5858becc590979a8
Adnan <adnan.muhib@rocketmail.com>
2018-12-04 15:36:51 +0500

    :ok_hand: ADD: Feature Point Class

---
+++ GaitRecognition/FeaturePoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaitRecognition
+{
+    class FeaturePoint
+    {
+        public Point point;
+        public string name;
+
+        public FeaturePoint() {
+            point = new Point();
+            name = "random";
+        }
+
+        public FeaturePoint(Point p, String n) {
+            point = new Point();
+            point.X = p.X;
+            point.Y = p.Y;
+            name = n;
+        }
+    }
+   
+}
